c284ac61012b52b3b3b71389080289e144f677ee
Criss <rcristian1986@gmail.com>
2022-08-10 22:46:29 -0300

    proyecto inicial

---
+++ src/main/java/com/example/matchpro/MatchproApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/matchpro/MatchproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

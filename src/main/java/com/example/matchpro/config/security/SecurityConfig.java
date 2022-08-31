c9f91d23b3b8b4dcb9e97398481f6b31e6175d04
Alben Bustamante <albenbustamante@gmail.com>
2022-08-30 19:31:19 -0500

    Añadido filtro a la seguridad

--- src/main/java/com/example/matchpro/config/security/SecurityConfig.java
+++ src/main/java/com/example/matchpro/config/security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.example.matchpro.config.security;
 
+import com.example.matchpro.config.security.jwt.JWTFilterRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Here is the main configuration for the security.
@@ -15,7 +19,10 @@ import org.springframework.security.web.SecurityFilterChain;
  * @author Alben Bustamante
  */
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JWTFilterRequest filterRequest;
 
     /**
      * @return a {@link BCryptPasswordEncoder} by default.
@@ -48,6 +55,10 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/v2/api-docs", "/configuration/ui", "/configuration/security").permitAll()
                 .antMatchers("/swagger-ui/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                .and()
+                .addFilterBefore(filterRequest, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .build();
     }
